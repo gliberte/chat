638bfa495c467f1a6673ed609032410ae07d2c18
Luis Solano <luis.solano.l@gmail.com>
2018-09-18 15:06:11 -0500

    ok

--- server/server.js
+++ server/server.js
@@ -1,10 +1,23 @@
 import express from 'express'
 const app = express()
+import http from 'http'
 import path from 'path'
+import socketIO from 'socket.io'
 
 const PORT = process.env.PORT || 7000
+
+const server = http.createServer(app)
+const io = socketIO(server)
+
 app.use(express.static(path.join(__dirname,'../client')))
 
-app.listen(PORT,()=>{
+io.on('connection',(socket)=>{
+    console.log("new user conected")
+    socket.on('disconnect',()=>{
+        console.log('user se desconecto')
+    })
+})
+
+server.listen(PORT,()=>{
     console.log(`SERVIDOR INICIADO EN ${PORT}`)
 })
