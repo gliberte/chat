1347edcf9478e50d070fa659776569da5061dfe6
Luis Solano <luis.solano.l@gmail.com>
2018-09-20 16:11:52 -0500

    ok

--- client/js/index.js
+++ client/js/index.js
@@ -2,10 +2,7 @@ const socket = io();
 socket.on('connect', () => {
     console.log("conectado")
 
-    socket.emit('createMessage',{
-        from:'luis@gmail.com',
-        text:'hola Luis'
-    })
+    
 })
 
 socket.on('disconnect', () => {
--- server/server.js
+++ server/server.js
@@ -16,14 +16,15 @@ io.on('connection',(socket)=>{
     socket.on('disconnect',()=>{
         console.log('user se desconecto')
     })
-    socket.emit('newMessage',{
-        from:'Luis',
-        text:'Hola luis',
-        createdAt:12345
-    })
+   
 
     socket.on('createMessage',(message)=>{
         console.log('createMessage',message)
+        io.emit('newMessage',{
+            from:message.from,
+            text:message.text,
+            createdAt:new Date().getTime()
+        })
     })
 })
 
